679f82ad90ece5cb90b18a377dcdf4852cf7e6b2
cibergarri <cibergarri@hotmail.com>
2017-02-05 19:49:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var Webtask = require('webtask-tools');
 var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+
+// expose this express app as a webtask-compatible function
+
+module.exports = Webtask.fromExpress(app);
+
+/*app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-});
+});*/
